0473aa343d0fbeb3ac619656ae1286401751c1ec
yuliansyahibr <questra1998@gmail.com>
2020-12-27 20:58:35 +0700

    update

--- dkantorpos/settings.py
+++ dkantorpos/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # DataFlair #Uploaded_Files
 MEDIA_URL = '/media/'
